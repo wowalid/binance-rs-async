ce9d2183acb1d7448be166a0f52169b889873176
gherk <walidarbel77@gmail.com>
2025-06-12 15:17:05 +0000

    save

--- src/client.rs
+++ src/client.rs
@@ -63,7 +63,7 @@ impl Client {
     pub async fn post_signed<T: DeserializeOwned>(&self, endpoint: &str, request: &str) -> Result<T> {
         let url = self.sign_request(endpoint, request);
 
-        println!("POST URL: {}", url);
+       
         let response = self.inner.post(&url).headers(self.build_headers(true)?).send().await?;
 
         self.handler(response).await
@@ -150,7 +150,7 @@ impl Client {
     ) -> Result<T> {
         let request = build_signed_request_p(payload, recv_window)?;
         let url = self.sign_request(endpoint, &request);
-        println!("PUT URL: {}", url);
+   
         let response = self.inner.put(&url).headers(self.build_headers(true)?).send().await?;
 
         self.handler(response).await
